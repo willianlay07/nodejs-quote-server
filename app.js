2404399604cf75e8e6de4af8ba49c43bee8e8c5e
willianlay07 <willianlay07@gmail.com>
2023-09-18 18:27:57 +0800

    Fixed issue

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
 import quoteRouter from "./route/quote.js";
+import cors from "cors";
 
 dotenv.config({
   path: "./config.env",
 });
-console.log(process.env.PORT);
+//console.log(process.env.PORT);
 
 const app = express();
+app.use(cors());
 app.use(express.json()); // Middleware to accept incoming
 
 app.get("/", (req, res) => {
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose
   });
 // Mongoose Connection -- END;
 
-console.log(process.env.NODE_ENV);
+//console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
